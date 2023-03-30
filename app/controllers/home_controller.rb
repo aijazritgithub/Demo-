a2226f7291e8689474d78736a3690adc0321f26c
aijaz.khan <aijazkhan2937@gmail.com>
2023-03-30 15:40:08 +0530

    Add App

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,23 @@
+class HomeController < ApplicationController
+  def index
+    @home = Home.all
+    render json: @home
+  end
+
+  def new; end
+
+  def create
+    @titles = Home.new(home_params)
+    if @titles.save
+      render json: { message: 'home is created successfully' }, status: 200
+    else
+      render json: { message: 'home is not created ' }
+    end
+  end
+
+  private
+
+  def home_params
+    params.permit(:name, :title)
+  end
+end
